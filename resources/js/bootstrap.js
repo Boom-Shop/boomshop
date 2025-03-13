819b94b69af30cf5acd61f021b40749baebcd28f
robypz <robertyepez0208@hotmail.com>
2025-03-13 10:10:35 +0100

    refactor: clean up bootstrap.js by removing unused imports and comments

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,21 +1,6 @@
-import _ from 'lodash';
-window._ = _;
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-
-
 
 import jQuery from 'jquery';
 window.$ = jQuery;
-
-/*import * as Popper from '@popperjs/core'
-window.Popper = Popper;*/
-
 
 import 'bootstrap';
 
@@ -23,21 +8,3 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-/*import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
-
-window.Pusher = Pusher;
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: false
-});*/
