3cfcc698d7adbd47786fba82336ab0485555201d
robypz <robertyepez0208@hotmail.com>
2025-03-14 14:04:37 +0100

    feat: add Laravel Echo and Reverb support for real-time broadcasting

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,3 @@
-
 import jQuery from 'jquery';
 window.$ = jQuery;
 
@@ -8,3 +7,11 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
