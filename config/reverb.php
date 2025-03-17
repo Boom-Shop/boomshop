883ce449892c333d7711f0eef751335ebda43845
robypz <robertyepez0208@hotmail.com>
2025-03-17 07:03:34 +0100

    feat: update .env.example and reverb configuration for email and Reverb integration

--- config/reverb.php
+++ config/reverb.php
@@ -34,7 +34,11 @@ return [
             'path' => env('REVERB_SERVER_PATH', ''),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [],
+                'tls' => [
+                    'local_cert' => env('REVERB_CERT', ''),
+                    'local_pk' => env('REVERB_PK', ''),
+                    'cafile' => env('REVERB_CAF', ''),
+                ],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
